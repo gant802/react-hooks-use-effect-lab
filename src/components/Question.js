dec1c9ba6510dafdb5b0b843c509cb76221737de
Grant Cummings <grantcummings4429@gmail.com>
2024-04-25 14:36:21 -0400

    finished lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    if(timeRemaining === 0) {
+      onAnswered(false)
+      setTimeRemaining(10)
+     }
+
+     const timerId =  setTimeout(() => {
+        setTimeRemaining((timeRemaining) => timeRemaining - 1); 
+      }, 1000);
+
+    // returning a cleanup function
+    return function cleanup() {
+      clearTimeout(timerId);
+    };
+  }, [timeRemaining]);
+
+  console.log(timeRemaining)
+  
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
